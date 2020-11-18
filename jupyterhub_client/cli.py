8a1b180f22bbe57161dd8c30c71aadcde03b75c4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2020-11-18 14:33:28 -0500

    Adding command line application for running notebook

--- jupyterhub_client/cli.py
+++ jupyterhub_client/cli.py
@@ -1,8 +1,38 @@
 import argparse
+import logging
+import asyncio
 
 
-def cli():
-    parser = argparse.ArgumentParser(description='JupyterHub traffic simulator')
-    parser.add_argument('username', type=str, help='username to simulate traffic')
-    parser.add_argument('--service-token', type=str, default=os.environ.get('JUPYTERHUB_TRAFFIC_SERVICE_TOKEN'), help='service token used to login to jupyterhub')
-    return parser.parse_args()
+def cli(args=None):
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description="jupyterhub client cli")
+    subparser = parser.add_subparsers(help="jupyterhub client cli")
+    create_run_subcommand(subparser)
+    parser.set_defaults(func=None)
+    args = parser.parse_args(args)
+
+    if args.func is None:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args.func(args)
+
+
+def create_run_subcommand(subparser):
+    subparser = subparser.add_parser("run")
+    subparser.add_argument("-u", "--username", type=str, help="username to run notebook as", required=True)
+    subparser.add_argument("-n", "--notebook", type=str, help="notebook to run", required=True)
+    subparser.add_argument("--hub", type=str, default="http://localhost:8000", help="url for running jupyterhub cluster")
+    subparser.set_defaults(func=handle_run)
+
+
+def handle_run(args):
+    from jupyterhub_client.execute import execute_notebook
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        execute_notebook(
+            hub_url=args.hub,
+            notebook_path=args.notebook,
+            username=args.username))
