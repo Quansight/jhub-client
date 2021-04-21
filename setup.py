8545ddebbf14d160c8621b97fc26a561df5bc6c5
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-04-21 15:17:31 -0400

    Bump verison to 0.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='jhub-client',
-    version='0.1.3',
+    version='0.1.4',
     description='Library and Client for managing, benchmarking, and interacting with jupyterhub',
     long_description=long_description,
     long_description_content_type='text/markdown',
